b6edd898774127c85a5c51c374dcfffd931aa796
EmreSimseker <Emre.Simseker@student.GildeOpleidingen.nl>
2026-09-16 16:40:03 +0200

    Initial commit

---
+++ src/datasources/db.datasource.ts
@@ -0,0 +1,26 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'lb4',
+  connector: 'postgresql',
+  url: 'postgresql://lb4_ua6w_user:oNa5MwERClmtGXXmfInBuXfSdcLFOGdR@dpg-dajt2ch594qs73dh4q8g-a.frankfurt-postgres.render.com/lb4_ua6w?ssl=true'
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class DbDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'db';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.db', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
